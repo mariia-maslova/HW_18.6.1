4d8cf43e3977e949a9f5e926be32f559fc519f70
mariia-maslova <79172732+mariia-maslova@users.noreply.github.com>
2023-05-11 09:57:01 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '5784845985:AAHtvgBI3Z3ADqXPC6jBhTcyf4UFYdP6_Sg'
+
+keys = {
+    'Рубль': 'RUB',
+    'Евро': 'EUR',
+    'Доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+}
